90c66f9da45bcc42e34d2a76327fca5cba21a372
kobe <kobe@vps-MacBook-Air.local>
2017-08-17 23:23:28 -0700

    functioning routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,31 +8,35 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   db.Burger.findAll({}).then(function(data) {
-    var hbsObject = { burgers: data };
-    res.render("index", hbsObject);
+    var burgObject = { burgers: data };
+    console.log(burgObject);
+    res.render("index", burgObject);
   });
 });
 
-router.post("/burgers/create", function(req, res) {
+router.post("/", function(req, res) {
   db.Burger
-    .create({ burger_name: req.body.burger_name, devoured: req.body.devoured })
+    .create(
+      { burger_name: req.body.burger_name, 
+        devoured: req.body.devoured})
     .then(function() {
       res.redirect("/burgers");
     });
 });
 
-router.put("/burgers/update/:id", function(req, res) {
- db.Burger
-    .update({ devoured: req.body.devoured }, { where: { id: req.params.id } })
+router.put("/:id", function(req, res) {
+  db.Burger
+    .update(
+      {devoured: req.body.devoured},
+      {where: {
+          id: req.params.id
+        }
+      })
     .then(function() {
-      res.redirect("/burgers");
+      res.redirect("/");
     });
 });
 
-router.delete("/burgers/delete/:id", function(req, res) {
-  models.burger.destroy({ where: { id: req.params.id } }).then(function() {
-    res.redirect("/burgers");
-  });
-});
+
 
 module.exports = router;
